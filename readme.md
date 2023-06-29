56da34a5dd991564877bad376528b660823f17f2
guilherme-antunes <112368848+guilherme-antunes@users.noreply.github.com>
2023-06-29 14:00:12 -0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,6 @@
+#####
+npx create-react-app alura-books
+npm start
+npm install styled-components
+caso n funcionar, instale a versão 5.3
+npm install styled-components@5.3.0
---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript Assertivo', id: 2, src:livro},
+    {nome: 'Cangaceiro do Javascript', id: 3, src:livro},
+    {nome: 'Aventureiro do Java', id: 4, src:livro}
+]
+
